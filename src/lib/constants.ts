605f08ec834e9ce57107adeff226078bad7de76a
Samuel Overington <soverington@expediagroup.com>
2023-07-22 15:58:00 +0100

    update constants for prod release

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,16 +1,21 @@
 import { join } from 'path'
 
+export const NODE_ENV = process.env.NODE_ENV || 'production'
+
 // Site info
-export const SITE_NAME = process.env.SITE_NAME || 'Next.js Starter'
-export const SITE_DESCRIPTION = process.env.SITE_DESCRIPTION || 'Next.js Starter'
-export const SITE_AUTHOR = process.env.SITE_AUTHOR || 'Next.js Starter'
+export const SITE_NAME = process.env.SITE_NAME || 'Blog [Samuel Overington]'
+export const SITE_DESCRIPTION = process.env.SITE_DESCRIPTION || 'A collection of my thoughts, ideas and learnings.'
+export const SITE_AUTHOR = process.env.SITE_AUTHOR || 'Samuel Overington'
 
 // Paths
-export const SITE_MENUS_PATH =process.env.SITE_MENUS_PATH ? join(process.cwd(), process.env.SITE_MENUS_PATH) : join(process.cwd(), 'src/_content/site-menus.yaml')
+export const SITE_MENUS_PATH = process.env.SITE_MENUS_PATH ? join(process.cwd(), process.env.SITE_MENUS_PATH) : join(process.cwd(), 'src/_content/site-menus.yaml')
 export const POSTS_PATH = process.env.POSTS_PATH ? join(process.cwd(), process.env.POSTS_PATH) : join(process.cwd(), './src/_posts')
 export const BLOG_SLUG = 'thoughts'
 
 // Site URL
-export const HOST = process.env.HOST || 'localhost'
-export const PORT = process.env.PORT || '3000'
-export const SITE_URL = process.env.SITE_URL || `${HOST}:${PORT}`
+export const HOSTNAME = process.env.HOSTNAME || 'localhost'
+// Default to no port
+export const PORT = process.env.PORT || ''
+
+// if process.env.SITE_URL is set, use that, otherwise use `${HOSTNAME}:${PORT}` if NODE_ENV is dev, otherwise use `${HOSTNAME}`
+export const SITE_URL = process.env.SITE_URL || (NODE_ENV === 'production' ? `https://${HOSTNAME}` : `http://${HOSTNAME}:${PORT}`)
--- src/lib/navigation.ts
+++ src/lib/navigation.ts
@@ -13,51 +13,16 @@ export type navItem ={
     basePath?: string
 }
 
-export function old_menuItemFactory( {text, href, slug, basePath} : navItem ): menuItem {
-    /**
-     * Construct a MenuItem object from a navItem object
-     * 
-     * @param text - text to display in the nav item
-     * @param href - href to link to
-     * @param slug - slug of the page
-     * @param basePath - base path of the page
-     * @returns MenuItem
-     * 
-     * if href is defined, return href
-     * if slug and optionally basePath are defined return path with format: {HOST}/{basePath}/{slug}
-     * if neither slug nor href are defined throw error
-     * 
-     * example:
-     * navItemFactory({'Home'}) => {text: 'Home', href: '/'}
-     * navItemFactory({'Tutorial',slug: 'tutorial', basePath: 'blog'}) => {text: 'Tutorial', href: 'https://{HOST}/blog/tutorial'}
-    */
-
-    if ( !slug && !href ) {
-        throw new Error('href or slug and basePath must be defined')
-    }
-    if (slug === '/' && !href) {
-        // replace slug with empty string to avoid double slashes
-        href = [SITE_URL, basePath].filter(Boolean).join('/')
-    }
-    return {
-        text: text,
-        href: (href) ? href : [process.env.HOST, basePath, slug].filter(Boolean).join('/')
-    }
-}
-
 export function menuItemFactory( {text, href, slug, basePath} : navItem ): menuItem {
 
     // remove leading and trailing slashes from slug
     slug = slug?.replace(/^\/|\/$/g, '')
     // remove leading and trailing slashes from basePath
     basePath = basePath?.replace(/^\/|\/$/g, '')
-    // http for localhost, https for production
-    const protocol = (process.env.NODE_ENV === 'production') ? 'https' : 'http'
 
     return {
         text: text,
-        // nb only one '/' after protocol
-        href: (href) ? href : [`${protocol}:/`, SITE_URL, basePath, slug].filter(Boolean).join('/'),
+        href: (href) ? href : [SITE_URL, basePath, slug].filter(Boolean).join('/'),
     }
 }
 
