6775862a8019223862e27079df9be000e99e5c66
Samuel Overington <samuel.overington@dimensionstudio.co>
2024-08-08 12:22:16 +0100

    burger to cross menu

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,16 +1,17 @@
 $(document).ready(function() {
-    const menuToggle = $('#menuToggle');
-    const menu = $('#menu');
-    const overlay = $('#overlay');
-  
-    menuToggle.on('click', function() {
-      menu.toggleClass('open');
-      overlay.toggleClass('show');
-      menuToggle.toggleClass('open');
-    });
-  
-    overlay.on('click', function() {
-      menu.removeClass('open');
-      overlay.removeClass('show');
-    });
+  const $menuToggle = $('#menuToggle');
+  const $menu = $('#menu');
+  const $overlay = $('#overlay');
+
+  $menuToggle.on('click', function() {
+    $menu.toggleClass('open');
+    $overlay.toggleClass('show');
+    $menuToggle.toggleClass('open');
+  });
+
+  $overlay.on('click', function() {
+    $menu.removeClass('open');
+    $overlay.removeClass('show');
+    $menuToggle.removeClass('open');
+  });
 });
